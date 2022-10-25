6b303ff5ca70ae37651991e0ac56df16d1c0029a
Nitai Seri <Nitai.ns@gmail.com>
2022-10-25 12:40:28 +0300

    commit before merge

--- backend/db/create_tables_queries.sql
+++ backend/db/create_tables_queries.sql
@@ -1,4 +1,6 @@
-CREATE TABLE User(
+USE ticket_arena;
+
+CREATE TABLE IF NOT EXISTS User(
     user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     full_name VARCHAR(30),
     passward VARCHAR(30),
@@ -6,12 +8,12 @@ CREATE TABLE User(
     rank INT 
 );
 
-CREATE TABLE Category(
+CREATE TABLE IF NOT EXISTS Category(
     category_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(20)
 );
 
-CREATE TABLE Ticket(
+CREATE TABLE IF NOT EXISTS Ticket(
     ticket_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     user_id INT REFERENCES User(user_id),
     title VARCHAR(50),
@@ -22,36 +24,36 @@ CREATE TABLE Ticket(
     date Date NOT NULL 
 );
 
-CREATE TABLE Transaction(
+CREATE TABLE IF NOT EXISTS Transaction(
     transaction_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     seller_id INT REFERENCES User(user_id),
     buyer_id INT REFERENCES User(user_id),
     status VARCHAR(20) 
 );
 
-CREATE TABLE Wish(
+CREATE TABLE IF NOT EXISTS Wish(
     wish_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     user_id INT REFERENCES User(user_id),
     location VARCHAR(30),
     date Date 
 );
 
-CREATE TABLE Ticket_tag(
+CREATE TABLE IF NOT EXISTS Ticket_tag(
     ticket_id INT REFERENCES Ticket(ticket_id),
     word VARCHAR(20) 
 );
 
-CREATE TABLE Wish_tag(
+CREATE TABLE IF NOT EXISTS Wish_tag(
     wish_id INT REFERENCES Wish(wish_id),
     word VARCHAR(20) 
 );
 
-CREATE TABLE User_ticket(
+CREATE TABLE IF NOT EXISTS User_ticket(
     user_id INT REFERENCES User(user_id),
     ticket_id INT REFERENCES Ticket(ticket_id)
 );
 
-CREATE TABLE User_wish(
+CREATE TABLE IF NOT EXISTS User_wish(
     user_id INT REFERENCES User(user_id),
     wish_id INT REFERENCES Wish(wish_id)
 );
