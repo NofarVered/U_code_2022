4bae262fde44e6f7d7db897c03c8ffded9a0b8f0
NofarVered <nofarss79@gmail.com>
2022-10-26 20:52:35 +0300

    queries for events

--- backend/db/delete_tabels.sql
+++ backend/db/delete_tabels.sql
@@ -1,11 +1,14 @@
 USE ticket_arena;
 
-drop table User;
-drop table Category;
-drop table Event;
-drop table Ticket;
+-- drop table Ticket;
+-- drop table Wish;
+-- drop table User_wish;
+drop table User_ticket;
 drop table Transaction;
-drop table Wish;
-drop table Event_tag;
-drop table Wish_tag;
-drop table User_wish;
+drop table ticket_tag;
+drop table Wish_tag; 
+
+-- drop table User;
+-- drop table Category;
+-- drop table Event;
+-- drop table Event_tag;
--- backend/db/queries/select_queries.py
+++ backend/db/queries/select_queries.py
@@ -1,15 +1,31 @@
 SELECT_ALL_EVENTS = "SELECT *\
                       FROM Event\
-                      ORDER BY date DESC;"
+                      ORDER BY date DESC\
+                      LIMIT 5;"
+
 SELECT_EVENTS_BY_CATEGORY = "SELECT *\
                             FROM Event\
                             WHERE category_id = (SELECT category_id\
                                                 FROM Category\
                                                 WHERE name = '{category_name}')\
                             ORDER BY date DESC;"
-SELECT_EVENTS_BY_TAGS = ""
-SELECT_EVENTS_BY_CATEGORY_AND_TAGS = ""
 
+SELECT_EVENTS_BY_TAG = "SELECT *\
+                        FROM Event\
+                        WHERE event_id = (SELECT event_id\
+                                            FROM event_tag\
+                                            WHERE word = '{word}')\
+                        ORDER BY date DESC;"
+
+SELECT_EVENTS_BY_CATEGORY_AND_TAGS = "SELECT *\
+                                    FROM Event\
+                                    WHERE event_id = (SELECT event_id\
+                                                    FROM event_tag\
+                                                    WHERE word = '{word}')\
+                                    AND category_id = (SELECT category_id\
+                                                FROM Category\
+                                                WHERE name = '{category_name}')\
+                                    ORDER BY date DESC;"
 
 SELECT_TICKET_BY_EVENT = "SELECT *\
                         FROM Ticket\
