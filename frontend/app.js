f706476ac922884a9055f1824d1c0972a411da15
NofarVered <nofarss79@gmail.com>
2022-10-26 15:06:55 +0300

    margin bottom search container

--- frontend/app.js
+++ frontend/app.js
@@ -2,13 +2,13 @@ const renderer = new Renderer();
 const ticketsClass = new TicketsModel();
 
 async function renderTickets() {
+  print("dddd");
   try {
     await ticketsClass.loadTickets();
     renderer.render(ticketsClass.getTickets());
   } catch (e) {
-    console.log(e)
-}
+    console.log(e);
+  }
 }
 
 renderTickets();
-
